59dce037f9864b9a81561e1a27b4cf4664bcea5d
chdder <3220725779@qq.com>
2019-05-21 23:40:31 +0800

    首次提交项目

---
+++ ppGPro/src/main/java/com/sender/Sender.java
@@ -0,0 +1,21 @@
+package com.sender;
+
+import com.connect.BaseConnector;
+
+import java.io.IOException;
+import java.io.Serializable;
+import org.apache.commons.lang.SerializationUtils;
+/**
+ *
+ */
+public class Sender extends BaseConnector {
+
+    public Sender(String queueName) throws IOException {
+        super(queueName);
+    }
+
+    public void sendMessage(Serializable object) throws IOException {
+        channel.basicPublish("",queueName, null, SerializationUtils.serialize(object));
+    }
+
+}
---
+++ ppGPro/src/main/java/com/test/Test.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import com.message.MessageInfo;
+import com.receiver.Receiver;
+import com.sender.Sender;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+        Receiver receiver = new Receiver("testQueue");
+        Thread receiverThread = new Thread(receiver);
+        receiverThread.start();
+        Sender sender = new Sender("testQueue");
+        for (int i = 0; i < 5; i++) {
+            MessageInfo messageInfo = new MessageInfo();
+            messageInfo.setChannel("test");
+            messageInfo.setContent("msg" + i);
+            sender.sendMessage(messageInfo);
+        }
+    }
+}
